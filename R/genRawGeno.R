94a8e80a6b419ca14b28a6dd6631d5af91cbd578
mxcai <32085863+mxcai@users.noreply.github.com>
2019-12-02 10:53:45 +0800

    Add files via upload

--- R/genRawGeno.R
+++ R/genRawGeno.R
@@ -4,8 +4,8 @@ genRawGeno <- function(maf, p, G, rho, n){
 
   X_tmp <- replicate(G,rmvnorm(n,mean=rep(0,p),sigma=sigma,method = "chol"))
   dim(X_tmp) <- c(n,p*G)
-  X <- matrix(1, n1+n2, p*G)
-  X[t(t(X_tmp) - quantile(X_tmp, maf^2)) < 0] <- 2
-  X[t(t(X_tmp) - quantile(X_tmp, 1-(1-maf)^2)) > 0] <- 0
+  X <- matrix(1, n, p*G)
+  X[t(t(X_tmp) - mapply(quantile,x=data.frame(X_tmp),probs=maf^2)) < 0] <- 2
+  X[t(t(X_tmp) - mapply(quantile,x=data.frame(X_tmp),probs=1-(1-maf)^2)) > 0] <- 0
   return(X)
 }
